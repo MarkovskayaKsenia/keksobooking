97798ee4635f034aac416add16a66b3ba5a7f42d
lubamark <lubamark@mail.ru>
2018-06-10 04:00:40 +0700

    fix renderCard(), add renderPin()

--- js/map.js
+++ js/map.js
@@ -50,9 +50,11 @@ var MAX_CHECK = 14;
 var MIN_GUESTS = 0;
 var MAX_GUESTS = 10;
 
+var PIN_WIDTH = 40;
+var PIN_HEIGHT = 40;
+
 // Функция для возвращения нового перемешанного массива
 var randomMixArray = function (arr) {
-  // я тут не была уверена, что можно менять исходный массив, поэтому сначала его скопировала, а потом перемешала
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
     newArr[i] = arr [i];
@@ -77,17 +79,22 @@ var getRandomFromArray = function (arr) {
 // Функция для генерации массива произвольной длины из другого массива
 var getRandomArrLength = function (arr) {
   var newArr = [];
-  for (var i = getRandomInt(1, arr.length); i >= 0; i--) {
+  for (var i = getRandomInt(0, arr.length - 1); i >= 0; i--) {
     newArr[i] = arr[i];
   }
-
   return newArr;
 };
 
+// Функции для определения положения метки
+var getPinX = function (x, pinWidth) {
+  return x - pinWidth / 2;
+};
+var getPinY = function (y, pinHeight) {
+  return y - pinHeight;
+};
+
 // Функция для генерации массива объектов объявлений
-var createAdList = function (quantity, titles, types, minX, minY, maxX, maxY, minPrice,
-    maxPrice, minRooms, maxRooms, minGuests,
-    maxGuests, minCheck, maxCheck, features, photos) {
+var createAdList = function (quantity, titles, types, minX, minY, maxX, maxY, minPrice, maxPrice, minRooms, maxRooms, minGuests, maxGuests, minCheck, maxCheck, features, photos, pinWidth, pinHeight) {
   var ads = [];
   var mixedTitles = randomMixArray(titles);
 
@@ -108,13 +115,13 @@ var createAdList = function (quantity, titles, types, minX, minY, maxX, maxY, mi
     ad.offer.guests = getRandomInt(minGuests, maxGuests);
     ad.offer.checkin = getRandomInt(minCheck, maxCheck) + ':00';
     ad.offer.checkout = getRandomInt(minCheck, maxCheck) + ':00';
-    ad.offer.features = getRandomArrLength(features);
+    ad.offer.features = getRandomArrLength(randomMixArray(features));
     ad.offer.description = '';
     ad.offer.photos = randomMixArray(photos);
 
     ad.location = {};
-    ad.location.x = x;
-    ad.location.y = y;
+    ad.location.x = getPinX(x, pinWidth);
+    ad.location.y = getPinY(y, pinHeight);
 
     ads.push(ad);
   }
@@ -122,57 +129,105 @@ var createAdList = function (quantity, titles, types, minX, minY, maxX, maxY, mi
   return ads;
 };
 
-
 // создаем массив объектов
-var adList = createAdList(QUANTITY, TITLES, TYPES, MIN_X, MIN_Y, MAX_X, MAX_Y, MIN_PRICE, MAX_PRICE, MIN_ROOMS, MAX_ROOMS, MIN_GUESTS, MAX_GUESTS, MIN_CHECK, MAX_CHECK, FEATURES, PHOTOS);
+var adList = createAdList(QUANTITY, TITLES, TYPES, MIN_X, MIN_Y, MAX_X, MAX_Y, MIN_PRICE, MAX_PRICE, MIN_ROOMS, MAX_ROOMS, MIN_GUESTS, MAX_GUESTS, MIN_CHECK, MAX_CHECK, FEATURES, PHOTOS, PIN_WIDTH, PIN_HEIGHT);
 
 // Показываем блок .map
 var mapWindow = document.querySelector('.map');
 mapWindow.classList.remove('map--faded');
 
-// Находим место для вставки пинов и шаблон
-var pinPlace = document.querySelector('.map__pins');
-var pinTemplate = document.querySelector('template')
+// Находим место для вставки карточки и шаблон
+var cardPlace = document.querySelector('.map__filters-container');
+var cardTemplate = document.querySelector('template')
   .content
   .querySelector('.map__card');
 
+// Функция для удаления дочерних элементов
+var removeChildren = function (list) {
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
+};
 
-// функция для отрисовки одного объявления.
-var renderPin = function (pin) {
-  var pinElement = pinTemplate.cloneNode(true);
-  pinElement.querySelector('.popup__avatar').setAttribute('src', pin.author.avatar);
-  pinElement.querySelector('.popup__title').textContent = pin.offer.title;
-  pinElement.querySelector('.popup__text--address').textContent = pin.offer.addres;
-  pinElement.querySelector('.popup__text--price').textContent = pin.offer.price;
-  pinElement.querySelector('.popup__text--price').insertAdjacentHTML('beforeend', ' &#x20bd;<span>/ночь</span>');
-  pinElement.querySelector('.popup__type').textContent = pin.offer.type;
-  pinElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + pin.offer.checkin + ', выезд до ' + pin.offer.checkout;
+// Функция для перевода типа апартаментов
+var localizeType = function (type) {
+  var localType = '';
+  switch (type) {
+    case 'flat' : localType = 'Квартира';
+      break;
+    case 'bungalo' : localType = 'Бунгало';
+      break;
+    case 'house' : localType = 'Дом';
+      break;
+    case 'palace' : localType = 'Дворец';
+      break;
+    default : localType = 'Хибарка';
+  }
 
-  var featureList = document.querySelector('.popup__features');
-  var features = document.querySelectorAll('.popup__feature');
+  return localType;
+};
 
-  // придумать как выводить только нужные li
+// Функция для отрисовки одного объявления.
+var renderCard = function (card) {
+  // обертка div для карточки, чтобы вытащить innerHTML
+  var overlay = document.createElement('div');
 
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__avatar').setAttribute('src', card.author.avatar);
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.addres;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price;
+  cardElement.querySelector('.popup__text--price').insertAdjacentHTML('beforeend', ' &#x20bd;<span>/ночь</span>');
+  cardElement.querySelector('.popup__type').textContent = localizeType(card.offer.type);
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
 
-  pinElement.querySelector('.popup__description').textContent = pin.offer.description;
+  var featureList = cardElement.querySelector('.popup__features');
+  var features = card.offer.features;
+  removeChildren(featureList);
+  for (var i = 0; i < features.length; i++) {
+    var newFeature = document.createElement('li');
+    newFeature.classList.add('popup__feature');
+    newFeature.classList.add('popup__feature--' + features[i]);
+    featureList.appendChild(newFeature);
+  }
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
 
-  var photoBlock = pinElement.querySelector('.popup__photos');
-  var defaultPhoto = pinElement.querySelector('.popup__photo');
+  var photoBlock = cardElement.querySelector('.popup__photos');
+  var defaultPhoto = cardElement.querySelector('.popup__photo');
   photoBlock.removeChild(defaultPhoto);
-  for (var i = 0; i < pin.offer.photos.length; i++) {
+  for (var j = 0; j < card.offer.photos.length; j++) {
     var newPhoto = defaultPhoto.cloneNode(defaultPhoto);
-    newPhoto.src = pin.offer.photos[i];
+    newPhoto.src = card.offer.photos[j];
     photoBlock.appendChild(newPhoto);
   }
+
+  overlay.appendChild(cardElement);
+  return overlay.innerHTML;
+};
+
+// Вставляем карту перед указанным блоком
+cardPlace.insertAdjacentHTML('beforebegin', renderCard(adList[0]));
+
+// // Находим место для вставки пинов и шаблон
+var pinsPlace = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('template')
+  .content
+  .querySelector('.map__pin');
+
+// Функция для отрисовки пина
+var renderPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = pin.offer.title;
 
   return pinElement;
 };
 
-// создаем фрагмент и добавляем в него элементы
+// Создаем фрагмент, добавляем в него пины, добавляем его в DOM
 var fragment = document.createDocumentFragment();
-
-fragment.appendChild(renderPin(adList[0]));
-
-
-pinPlace.appendChild(fragment);
-console.log(adList);
+for (var i = 0; i < adList.length; i++) {
+  var pin = renderPin(adList[i]);
+  fragment.appendChild(pin);
+}
+pinsPlace.appendChild(fragment);
