b517aa808826651ce27f24e77f8808a7d0c3fa79
lubamark <lubamark@tpu.ru>
2018-07-02 16:51:54 +0700

    delete unuse code from utils

--- js/utils.js
+++ js/utils.js
@@ -2,27 +2,6 @@
 
 (function () {
   window.utils = {
-    // Функция для возвращения нового перемешанного массива
-    randomMixArray: function (arr) {
-      var newArr = arr.slice(0);
-
-      newArr.sort(function () {
-        return 0.5 - Math.random();
-      });
-      return newArr;
-    },
-    // Функция для генерации случайных чисел в интервале
-    getRandomInt: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    // Функция для генерации массива произвольной длины из другого массива
-    getRandomArrLength: function (arr) {
-      var newArr = [];
-      for (var i = window.utils.getRandomInt(0, arr.length - 1); i >= 0; i--) {
-        newArr[i] = arr[i];
-      }
-      return newArr;
-    },
     // Функции для определения положения метки
     getPinX: function (x, pinWidth) {
       return x + pinWidth / 2;
